8b5b47149d7350ea2d0c054d73401db4a0a0ba41
Jahongir Qurbonov <qurbonovjahongir17@gmail.com>
2023-04-16 18:31:52 +0500

    fix file range

--- tasks/utils.py
+++ tasks/utils.py
@@ -29,39 +29,3 @@ def cal_key(obj, m, content_type_=False, object_id_=False):
         return present_keys[0] + 1
     else:
         return 1
-
-
-from django.http import FileResponse
-
-
-def download_file(request, filefield):
-    # Open the file
-    with filefield.open() as file:
-        # Read the file content
-        file_content = file.read()
-
-        # Get the range from the headers
-        range_header = request.META.get("HTTP_RANGE")
-        if range_header is None:
-            # If no range is provided, send the entire file
-            response = FileResponse(file, filename=filefield.path)
-        else:
-            # If a range is provided, send the specified range
-            start_range, end_range = range_header.split("=")[1].split("-")
-            start_range = int(start_range)
-            end_range = int(end_range) if end_range else len(file_content) - 1
-            content_range = f"bytes {start_range}-{end_range}/{len(file_content)}"
-            response = FileResponse(
-                file_content[start_range : end_range + 1],
-                content_type="application/octet-stream",
-                filename=filefield.path,
-            )
-            response["Content-Range"] = content_range
-
-        # Set the content length header
-        # response["Content-Length"] = str(len(file_content))
-
-    # Set the content disposition header
-    response["Content-Disposition"] = f'attachment; filename="{filefield.name}"'
-
-    return response
